bb05ea129af962e176d78ea56f68aa538be7cb3f
zoar2307 <zoar2307@gmail.com>
2024-10-14 15:46:59 +0300

    Add SideBar

--- apps/mail/cmps/SideBar.jsx
+++ apps/mail/cmps/SideBar.jsx
@@ -12,7 +12,15 @@ export function SideBar() {
 
     return (
         <div className="side-bar">
+            <div className="compose-container">
+                <button className="compose-btn">New</button>
+            </div>
 
+            <button className="btn"><div></div> <span>Inbox</span><span>number</span></button>
+            <button className="btn">Stared</button>
+            <button className="btn">Sent</button>
+            <button className="btn">Drafts</button>
+            <button className="btn">Trash</button>
         </div>
     )
 }
