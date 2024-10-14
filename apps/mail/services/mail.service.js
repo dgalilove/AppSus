3724dee2ec81640c9e5a74cdbe50bc899f62e77b
zoar2307 <zoar2307@gmail.com>
2024-10-14 10:42:12 +0300

    Add start service modal

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,1 +1,28 @@
 // mail service
+
+// Modal sample
+
+// const mail = {
+//     id: 'e101',
+//     createdAt: 1551133930500,
+//     subject: 'Miss you',
+//     body: 'Would love to catch up sometimes',
+//     isRead: false,
+//     sentAt: 1551133930594,
+//     removeAt: null,
+//     from: 'momo@momo.com',
+//     to: 'user@appsus.com'
+// }
+
+const loggedUser = {
+    email: 'user@appsus.com',
+    fullName: 'Eli Copter'
+}
+
+const filterBy = {
+    status: 'inbox',// sent/trash/draft
+    txt: 'puki',
+    isRead: true,
+    isStared: true,
+    labels: []
+}
