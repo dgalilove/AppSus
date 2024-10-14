be28ce16b034385fbe2462758f29c9133ba4c523
zoar2307 <zoar2307@gmail.com>
2024-10-14 11:47:21 +0300

    Add mail list render

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,3 +1,17 @@
-export function MailList() {
-    return <div>Mail list</div>
+import { MailPreview } from "./mailPreview.jsx"
+
+export function MailList({ mailList }) {
+    if (!mailList) return <div>Loading...</div>
+    return <div className="mail-list">
+        <ul>
+            {mailList.map(mail => {
+                return (
+                    <li key={mail.id}>
+                        <MailPreview mail={mail} />
+                    </li>
+                )
+            })}
+        </ul>
+
+    </div>
 }
--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -1,5 +1,30 @@
+import { MailList } from "../cmps/MailList.jsx"
+import { mailService } from "../services/mail.service.js"
+
+const { useState, useEffect, useRef } = React
 
 export function MailIndex() {
-    return <div>mail app</div>
+
+    const [mails, setMails] = useState('')
+    const [filterBy, setFilterBy] = useState({})
+
+    useEffect(() => {
+        loadMails()
+    }, [])
+
+    function loadMails() {
+        mailService.query(filterBy)
+            .then(mails => {
+                setMails(mails)
+                console.log(mails)
+            })
+            .catch(err => console.log(err))
+
+    }
+
+    if (!mails) return <div>Loading...</div>
+    return <div className="main-index">
+        <MailList mailList={mails} />
+    </div>
 }
 
