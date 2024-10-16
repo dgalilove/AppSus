20fa110c2d7595ed36c7bcb9a195734539f2130c
dgalilove <dgalilove@gmail.com>
2024-10-16 15:41:27 +0300

    Add nav and css to home

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,6 +1,16 @@
+const { Link } = ReactRouterDOM;
 
 export function Home() {
-    return <section className="home">
-        <h1>Welcome to home page!</h1>
-    </section>
+    return (
+        <section className="home">
+            <nav>
+                <Link to="/note">
+                    <img src="assets/css/apps/note/note-svgs/icons8-google-keep.svg" alt="" />
+                </Link>
+                <Link to="/mail/inbox">
+                    <img src="assets/css/apps/mail/mail-svgs/icons8-gmail.svg" alt="" />
+                </Link>
+            </nav>
+        </section>
+    );
 }
