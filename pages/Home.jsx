9eb7b0a09ba52872498b4967494042b454cb25b2
dgalilove <dgalilove@gmail.com>
2024-10-17 16:36:34 +0300

    Home edit

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -4,12 +4,23 @@ export function Home() {
     return (
         <section className="home">
             <nav>
-                <Link to="/note">
-                    <img src="assets/css/apps/note/note-svgs/icons8-google-keep.svg" alt="" />
-                </Link>
-                <Link to="/mail/inbox">
-                    <img src="assets/css/apps/mail/mail-svgs/icons8-gmail.svg" alt="" />
-                </Link>
+                <div className='header-title'>
+
+                    <h2>App Sus</h2>
+                    <h3>Zohar & Daniel</h3>
+                </div>
+                <div className='apps-nav'>
+                    <Link to="/note">
+                        <img src="assets/css/apps/note/note-svgs/icons8-google-keep.svg" alt="" />
+                    </Link>
+                    <Link to="/mail/inbox">
+                        <img src="assets/css/apps/mail/mail-svgs/icons8-gmail.svg" alt="" />
+                    </Link>
+                </div>
+                <div className='pixel-char'>
+                    <img src="assets/css/apps/note/note-svgs/pixel-daniel.png" alt="" />
+                    <img src="assets/css/apps/note/note-svgs/pixel-zohar.png" alt="" />
+                </div>
             </nav>
         </section>
     );
